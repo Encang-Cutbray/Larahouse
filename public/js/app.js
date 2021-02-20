3e626c17700a1f81289a3c60fcac7809075e04d9
encang cutbray <cabe2cabean214@gmail.com>
2021-02-21 00:03:05 +0700

    app js compile asset

--- public/js/app.js
+++ public/js/app.js
@@ -6847,7 +6847,6 @@ new vue__WEBPACK_IMPORTED_MODULE_2__.default({
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var laravel_echo__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! laravel-echo */ "./node_modules/laravel-echo/dist/echo.js");
-/* provided dependency */ var process = __webpack_require__(/*! process/browser */ "./node_modules/process/browser.js");
 window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6884,9 +6883,9 @@ window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__.default({
   key: "websockets",
   // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
   // forceTLS: true
-  namespace: process.env.MIX_NAMESPACE,
-  wsHost: process.env.MIX_WS_HOST,
-  wsPort: process.env.MIX_WS_PORT,
+  namespace: "Warehouse.Example.Events",
+  wsHost: "websockets.ps",
+  wsPort: "6060",
   forceTLS: false,
   disableStats: true
 });
